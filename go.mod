cd2a8c68d41d289106bfa554149962eccb3ff65f
nithin <nithin@skartio.ai>
2025-07-17 18:51:46 +0530

    added router gorilla

---
+++ go.mod
@@ -0,0 +1,5 @@
+module Golang-server-node-red
+
+go 1.24.5
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- server.go
+++ server.go
@@ -2,9 +2,30 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println("hello go")
+
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, "Hello, welcome to my website: %s\n", r.URL.Query().Get("token"))
+	// })
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static", fs))
+
+	http.ListenAndServe(":8080", r)
 
 }
